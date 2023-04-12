e060de286eded34a69e4f24bdd806c8db67201cd
“DNSK_MAC” <sslgh1024@naver.com>
2023-04-12 19:17:27 +0900

    install soynlp

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from konlpy.tag import Okt
+from soynlp.noun import LRNounExtractor
 
 data = pd.read_csv("train.csv")
 
@@ -15,9 +16,9 @@ t_df = t_df[['text', 'hate']]
 t_df['ko_text'] = t_df['text'].str.replace(
     pat=r'[^ ㄱ-ㅣ가-힣]+', repl=r'', regex=True)
 
-okt = Okt()
+noun_extractor = LRNounExtractor()
 
 
-print(okt.pos("동해물과 백두산이 마르고 닳도록"))
+
 
 print(t_df)
